9f48f0cef4f58b6d3e02cd05325e90cdca8d7288
Mark De Guzman <siopaomark@gmail.com>
2024-02-27 19:37:28 +0800

    Complete skeleton of dashboard

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
     <div class="wrapper">
         <aside id="sidebar">
             <div class="d-flex">
-                <button class="toggle-btn" type="button">
+                <button id="toggle-btn" type="button">
                     <i class="fa-solid fa-grip-vertical"></i>
                 </button>
                 <div class="sidebar-logo">
@@ -62,7 +62,7 @@
                         <i class="fa-solid fa-table-cells-large"></i>
                         <span>Multi Level</span>
                     </a>
-                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar">
+                    <ul id="multi" class="sidebar-dropdown list-unstyled collapse" data-bs-parent="#sidebar"> 
                         <li class="sidebar-item">
                             <a href="" class="sidebar-link collapsed" data-bs-toggle="collapse" data-bs-target="#multi-two" aria-expanded="false" aria-controls="multi-two">
                                 Two Links
@@ -105,7 +105,7 @@
         <!---MAIN SCREEN STARTS--->
 
         <div class="main">
-            <nav class="navbar navbar-expand px4 py3">
+            <nav class="navbar navbar-expand px-4 py-3">
                 <form action="" class="d-none d-sm-inline-block">
                     <div class="input-group input-group-navbar">
                         <input type="text" class="form-control border-0 rounded" placeholder="Search...">
@@ -118,7 +118,7 @@
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item dropdown">
                             <a href="#" class="nav-icon pe-md-0" data-bs-toggle="dropdown">
-                                <img src="images/gelo-1x1.jpg" class="img-fluid" alt="">
+                                <img src="images/gelo-1x1.jpg" class="avatar img-fluid" alt="">
                             </a>
                             <div class="dropdown-menu dropdown-menu-end rounded">
                                 <a href="#" class="dropdown-item">
@@ -134,17 +134,141 @@
                     </ul>
                 </div>
             </nav>
+            <main class="content px-3 py-4">
+                <div class="container-fluid">
+                    <div class="mb-3">
+                        <h3 class="fw-bold fs-4 mb-3">Admin Dashboard</h3>
+                        <div class="row">
+                            <div class="col-12 col-md-4">
+                                <div class="card border-0">
+                                    <div class="card-body py-4">
+                                        <h5 class="mb-2 fw-bold">
+                                            Members Progress
+                                        </h5>
+                                        <p class="mb-2 fw-bold">
+                                            PHP 72,540
+                                        </p>
+                                        <div class="mb-0">
+                                            <span class="badge text-success me-2">
+                                                +9.0%
+                                            </span>
+                                            <span class="fw-bold">
+                                                Since Last Month
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="card border-0">
+                                    <div class="card-body py-4">
+                                        <h5 class="mb-2 fw-bold">
+                                            Members Progress
+                                        </h5>
+                                        <p class="mb-2 fw-bold">
+                                            PHP 72,540
+                                        </p>
+                                        <div class="mb-0">
+                                            <span class="badge text-success me-2">
+                                                +9.0%
+                                            </span>
+                                            <span class="fw-bold">
+                                                Since Last Month
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4">
+                                <div class="card border-0">
+                                    <div class="card-body py-4">
+                                        <h5 class="mb-2 fw-bold">
+                                            Members Progress
+                                        </h5>
+                                        <p class="mb-2 fw-bold">
+                                            PHP 72,540
+                                        </p>
+                                        <div class="mb-0">
+                                            <span class="badge text-success me-2">
+                                                +9.0%
+                                            </span>
+                                            <span class="fw-bold">
+                                                Since Last Month
+                                            </span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                            <h3 class="fw-bold fs-4 my-3">
+                                Avg. Agent Earnings
+                            </h3>
+                            <div class="row">
+                                <div class="col-12">
+                                    <table class="table table-striped">
+                                        <thead>
+                                            <tr class="highlighted">
+                                                <th scope="col">#</th>
+                                                <th scope="col">First</th>
+                                                <th scope="col">Last</th>
+                                                <th scope="col">Handle</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <th scope="row">1</th>
+                                                <td>Mark</td>
+                                                <td>Otto</td>
+                                                <td>@mdo</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">2</th>
+                                                <td>Jacob</td>
+                                                <td>Thornton</td>
+                                                <td>@fat</td>
+                                            </tr>
+                                            <tr>
+                                                <th scope="row">3</th>
+                                                <td colspan="2">Larry the Bird</td>
+                                                <td>@twitter</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+
+            <footer class="footer">
+                <div class="container-fluid">
+                    <div class="row text-body-secondary">
+                        <div class="col-6 text-start">
+                            <a href="#" class="text-body-secondary">
+                                <strong>PITI CDP</strong>
+                            </a>
+                        </div>
+                        <div class="col-6 text-end text-body-secondary d-none d-md-block">
+                            <ul class="list-inline mb-0">
+                                <li class="list-inline-item">
+                                    <a href="" class="text-body-secondary">Contact</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="" class="text-body-secondary">About Us</a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a href="" class="text-body-secondary">Terms & Conditions</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
 
         <!---MAIN SCREEN END--->
     </div>
-
-
-
-
-
-
-
 
     <!--Scripts-->
 
